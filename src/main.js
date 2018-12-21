c1ddbe341430bfd9e267c691e3d1441366e9d31d
vinhle.bigin <vinh.le@bigin.vn>
2018-12-21 17:26:07 +0700

    checkiin

--- src/main.js
+++ src/main.js
@@ -1,78 +1,48 @@
 import './style.scss';
 import Vue from 'vue';
-import genres from './util/genres';
 
+import VueResource from 'vue-resource'
+import moment from 'moment-timezone'
+//component
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+Vue.use(VueResource);
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype,'$moment',{get(){
+
+    console.log('test');
+    console.log(this);
+    console.log(this.$root);
+    return this.$root.moment;thuytrang.mis93@gmail.com
+}});
 new Vue({
     el:'#app',
-
+    data:{
+        genre:[],
+        time:[],
+        movies:[],
+        moment,
+        day:moment(),
+    },
     methods: {
-        checkFilter () {
-            alert('hello parent')
+        checkFilter (category,title,checked) {
+            if(checked){
+                this[category].push(title)
+            }else{
+                let index =this[category].indexOf(title);
+                if(index>-1) this[category].splice(index,1);
+            }
         }
     },
     components:{
-        'movie-list':{
-            template:`<div id="movie-list">
-                        <div v-for=" item in movies" class="movie">{{item.title}}</div>
-                    </div>`,
-                    data(){
-                        return {
-                            movies:[
-                                {title:'Pulp fiction'},
-                                {title:'Home fiction'},
-                                {title:'Austin fiction'},
-                                {title:'Pulp fiction'},
-                            ]
-                        }
-                    }
-        },
-        'movie-filter':{
-            data(){
-                return {
-                    genres
-                };
-            },
-            methods: {
-                checkFilter () {
-                    alert('hello')
-                }
-            },
-            template:`<div id="movie-filter">
-                        <h2>Filter results</h2>
-                        <div class="filter-group">
-                            <div v-for="genre in genres">
-                                {{genre}} 
-                            </div>
-                            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
-                        </div>
-                    </div>`,
-                    components: {
-                        'check-filter':{
-                            data(){
-                                return{
-                                    checked:false
-                                }
-                            },
-                            props:['title'],
-                            methods: {
-                                checkFilter () {
-                                    this.checked=!this.checked
-                                    this.$emit('check-filter')
-                                }
-                            },
-                            template:`
-                                <div v-bind:class="{'check-filter':true , active : checked }" v-on:click="checkFilter"> 
-                                    <span class="checkbox"></span>
-                                    <span class="check-filter-title">
-                                        {{title}} 
-                                    </span>
-                                </div>                               
-                            `
-                        },
-                    }
-
-        }
+        MovieList,
+        MovieFilter,
+    },
+    created(){
+        this.$http.get('/api').then(res=>{
+            this.movies = res.data;
+        });
     }
     
 });
-console.log('hello')
